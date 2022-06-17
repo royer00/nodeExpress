622d58b9087159fad4e3fc33b0059f68af572991
Kevin Royer <royerkevin@outlook.com>
2022-06-17 13:27:20 -0500

    Express Router

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const morgan = require('morgan')
+const campsiteRouter = require('./routes/campsiteRouter')
 
 const hostname = 'localhost'
 const port = 3000
 
 const app = express()
 app.use(morgan('dev'))
+app.use(express.json())
+
+app.use('/campsites', campsiteRouter)
+
+//Campsite CRUD with campsiteId
+// app.get('/campsites/:campsiteId', (req, res) => {
+//     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`)
+// })
+
+// app.post('/campsites/:campsiteId', (req, res) => {
+//     res.statusCode = 403;
+//     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+// })
+
+// app.put('/campsites/:campsiteId', (req, res) => {
+//     res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
+//     res.end(`Will update the campsite: ${req.body.name}
+//         with description: ${req.body.description}`)
+// })
+
+// app.delete('/campsites/:campsiteId', (req, res) => {
+//     res.end(`Deleting campsite: ${req.params.campsiteId}`)
+// })
+
 
 
 app.use(express.static(__dirname + '/public'))
@@ -15,6 +40,10 @@ app.use((req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.end('<html><body><h1>This is an express server</h1></body></html>')
 })
+
+
+
+
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
