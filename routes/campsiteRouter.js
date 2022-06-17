eec7151d97c6d420f15e2fc814e1dbec8108c54b
Kevin Royer <royerkevin@outlook.com>
2022-06-17 14:02:01 -0500

    Workshop 1 Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const campsiteRouter = express.Router()
 
+//  /campsites
 campsiteRouter.route('/')
 .all((req, res, next) =>{
     res.statusCode = 200
@@ -20,6 +21,25 @@ campsiteRouter.route('/')
 .delete((req, res) => {
     res.end("Deleting all campsites")
 })
+
+// campsites/campsiteId
+campsiteRouter.route('/:campsiteId')
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`)
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`)
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`)
+})
+
 
 
 
